0286334f79d0339e5d0a62146af3a584a8cc8c95
nisargdpatel <46580820+nisargdpatel@users.noreply.github.com>
2020-10-21 10:33:08 -0400

    Contact class created
    This will a structure of contact objects and will be used to create database

---
+++ ContactManagemenSystem/ContactManagemenSystem/Data/Contact.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactManagemenSystem.Data
+{
+    public class Contact
+    {
+        [Key]
+        public int Id { get; set; }     //Primary key - Contact Id
+
+        [Required]
+        public string FirstName { get; set; }       //Required variable - First Name
+
+        [Required]
+        public string LastName { get; set; }        //Required variable - Last Name
+
+        [Required]
+        [Phone]
+        public string PhoneNumber { get; set; }     //Required variable - Phone Number
+
+        [Required]
+        public DateTime BirthDate { get; set; }     //Required variable - Birth Date
+    }
+}
